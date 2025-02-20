5a7007f4e5a8b2fc4c713f2b34783782941b89b8
Codesmith28 <sarthaksiddhpura@gmail.com>
2025-02-20 09:50:42 +0530

    refactor: remove unused import in encryption module

--- src/encryption/encrypt.rs
+++ src/encryption/encrypt.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 // list of numbers:
 pub static FIBBONACI_NUMBERS: [i32; 20] = [
     0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181,
