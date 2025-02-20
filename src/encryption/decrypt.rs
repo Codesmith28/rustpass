483c125b8f81aa36a9484c68db0b58dbcce9d425
Codesmith28 <sarthaksiddhpura@gmail.com>
2025-02-20 09:14:03 +0530

    feat: implement Fibonacci-based encryption and decryption

--- src/encryption/decrypt.rs
+++ src/encryption/decrypt.rs
@@ -1,1 +1,10 @@
+use super::encrypt::FIBBONACI_NUMBERS;
 
+pub fn fibbil_unhash(hash: &str) -> String {
+    let mut un_hash = String::new();
+    for char in hash.chars() {
+        let shifted = ((char as u32) - (FIBBONACI_NUMBERS[char as usize] as u32)) as u8;
+        un_hash.push(shifted as char);
+    }
+    un_hash
+}
--- src/encryption/encrypt.rs
+++ src/encryption/encrypt.rs
@@ -1,4 +1,13 @@
-// salting
+// list of numbers:
+pub static FIBBONACI_NUMBERS: [i32; 20] = [
+    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181,
+];
 
-// some custom hashing algorithm
-
+pub fn fibbil_hash(string: &str) -> String {
+    let mut hash = String::new();
+    for char in string.chars() {
+        let shifted = ((char as u32) + (FIBBONACI_NUMBERS[char as usize] as u32)) as u8;
+        hash.push(shifted as char);
+    }
+    hash
+}
